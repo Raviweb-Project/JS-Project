4dbd0cf20e406c6e8c3bdbabab01775893691f4c
Ravi Shankar Singh <raviwebdesigning2024@gmail.com>
2024-07-20 09:12:36 +0530

    Division in js Simple Code

---
+++ Division.js
@@ -0,0 +1,9 @@
+for(let num=1; num<=100; num++){
+    if(num%2==0){
+        console.log("Div By 2",num);
+    }else if(num%3==0){
+        console.log("Div By 3", num);
+    }else{
+            console.log("inveid");
+    }
+}
