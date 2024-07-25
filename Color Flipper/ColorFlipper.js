e56edb17be3c78d35fa038c8f7852a3457f93b73
Raviweb-Project <Bholasinghcs12@gmail.com>
2024-07-25 17:29:18 +0530

    Delete Color Flipper directory

--- Color Flipper/ColorFlipper.js
+++
@@ -1,9 +0,0 @@
-const colors = ["#F0F8FF", "#33FF57", "#3357FF", "#F0E68C", "#8A2BE2", "#FF69B4"];
-const button = document.querySelector('.Color-box');
-
-button.addEventListener('click', () => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
-
-
